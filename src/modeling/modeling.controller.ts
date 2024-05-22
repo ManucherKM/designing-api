110e8af9494cd5d83b7e3d7bf89cdc37b98d7259
244 <244@first.local>
2024-05-22 13:40:32 +0500

    .

--- src/modeling/modeling.controller.ts
+++ src/modeling/modeling.controller.ts
@@ -31,12 +31,34 @@ export class ModelingController {
   }
 
   @Patch(':id')
+  @UseInterceptors(SerializerInterceptor(Modeling))
   async update(
     @Param('id') id: string,
     @Body() updateModelingDto: UpdateModelingDto,
   ) {
     try {
-      return await this.modelingService.update(id, updateModelingDto);
+      const isUpdated = await this.modelingService.update(
+        id,
+        updateModelingDto,
+      );
+
+      if (!isUpdated) {
+        throw new HttpException(
+          { message: 'Failed to update the model.' },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      const foundModel = await this.modelingService.findById(id);
+
+      if (!foundModel) {
+        throw new HttpException(
+          { message: 'Failed to retrieve the model.' },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      return foundModel;
     } catch (e) {
       throw new HttpException(
         { message: e.message },
